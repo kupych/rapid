5266873cd5468593ad3338b57a353a349481e9c0
Stefan Kupych <stefan@kupy.ch>
2025-11-17 21:03:31 -0500

    v0.2.6 - Response history, last command, status colors

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 
 	args := flag.Args()
 	if len(args) < 1 {
-		fmt.Println("RAPID v0.2.4 - Rapid API Dialogue")
+		fmt.Println("RAPID v0.2.6 - Rapid API Dialogue")
 		fmt.Println("Usage: rapid [--debug] <base-url>")
 		fmt.Println()
 		fmt.Println("Warning: this is a WIP. More functionality coming soon.")
